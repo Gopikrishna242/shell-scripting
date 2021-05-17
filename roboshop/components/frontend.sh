b06ffd0df8b892d3f6efd8920263f4ec3f6efc95
Gopikrishna242 <82368153+Gopikrishna242@users.noreply.github.com>
2021-05-17 11:46:55 +0530

    Update frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -22,9 +22,9 @@ Stat $?
 
 Head "Update Nginx Configuration"
 mv roboshop.conf /etc/nginx/sites-enabled/roboshop.conf
-for comp in catalogue cart user shipping payment ; do
-  sed -i -e "/$comp/ s/localhost/${comp}.ec2-54-90-123-13.compute-1.amazonaws.com/" /etc/nginx/sites-enabled/roboshop.conf
-done
+#for comp in catalogue cart user shipping payment ; do
+  #sed -i -e "/$comp/ s/localhost/${comp}.ec2-54-90-123-13.compute-1.amazonaws.com/" /etc/nginx/sites-enabled/roboshop.conf
+#done
 Stat $?
 
 Head "Restart Nginx Service"
